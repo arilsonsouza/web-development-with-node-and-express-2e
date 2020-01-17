65531fe5f809b31ec02debd2d2794dc7659cd2c7
Arilson Souza <arilsons61@gmail.com>
2020-01-16 23:20:22 -0300

    ch03 - Static Files and Views

--- ch03/02-meadowlark.js
+++ ch03/02-meadowlark.js
@@ -14,17 +14,19 @@ app.get('/', (req, res) => res.render('home'))
 
 app.get('/about', (req, res) => res.render('about'))
 
+app.use(expess.static(__dirname + '/public'))
+
 // custom 404 page
 app.use((req, res) => {
   res.status(404)
-  res.send('404')
+  res.render('404')
 })
 
 // custom 500 page
 app.use((err, req, res, next) => {
   console.error(err.message)  
   res.status(500)
-  res.send('500')
+  res.render('500')
 })
 
 app.listen(port, () =>
