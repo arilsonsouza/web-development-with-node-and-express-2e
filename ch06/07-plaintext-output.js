e33881173fc651add16a83d90f29acedfc33bf3b
Arilson Souza <arilsons61@gmail.com>
2020-01-18 14:51:33 -0300

    ch06 - plaintext output

---
+++ ch06/07-plaintext-output.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.disable('x-powered-by')
+
+app.get('/text', (req, res) => {
+  res.type('text/plain')
+  res.send('this is a test')
+})
+
+app.get('*', (req, res) => res.send('Check out the "<a href="/text">plain text</a>" page!'))
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}/text\n`))
