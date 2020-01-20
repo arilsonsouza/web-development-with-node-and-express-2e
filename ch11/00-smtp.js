d8db5b35d0fe5888386745407876c5a4e7b169a0
Arilson Souza <arilsons61@gmail.com>
2020-01-19 22:29:15 -0300

    ch11 - Nodemailer

---
+++ ch11/00-smtp.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer')
+
+const { credentials } = require('./config')
+console.log('CREDENTIALS: ', credentials)
+const mailTransport = nodemailer.createTransport({
+  host: 'smtp.sendgrid.net',
+  auth: {
+    user: credentials.sendgrid.user,
+    pass: credentials.sendgrid.password
+  }
+})
+
+async function main () {
+  try {
+    const result = await mailTransport.sendMail({
+      from: '"Meadowlark Travel" <info@meadowlarktravel.com>',
+      to: 'joecustomer@gmail.com',
+      subject: 'Your Meadowlark Travel Tour',
+      text:  'Thank you for booking your trip with Meadowlark Travel.  ' +
+      'We look forward to your visit!'
+    })
+    console.log('mailt sent successfully: ', result)
+  } catch (err) {
+    console.log('could not send email: ' + err.message)
+  }
+}
+
+main()
