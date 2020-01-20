23da367b89bb2a8cd54c350409df60c045668c7f
Arilson Souza <arilsons61@gmail.com>
2020-01-19 22:49:32 -0300

    ch11 - html email

---
+++ ch11/03-html-email.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer')
+
+const { credentials } = require('./config')
+
+const mailTransport = nodemailer.createTransport({
+  host: 'smtp.sendgrid.net',
+  auth: {
+    user: credentials.sendgrid.user,
+    pass: credentials.sendgrid.password
+  }
+})
+
+async function main () {
+  try {
+    const result = await mailTransport.sendMail({
+      from: '"Meadowlark Travel" <info@meadowlarktravel.com>',
+      to: 'joe@gmail.com, "Jane Customer" <jane@yahoo.com>, fred@hotmail.com',
+      subject: 'Your Meadowlark Travel Tour',
+      html: '<h1>Meadowlark Travel</h1>\n<p>Thanks for book your trip with ' +
+      'Meadowlark Travel.  <b>We look forward to your visit!</b>',
+      text:  'Thank you for booking your trip with Meadowlark Travel.  ' +
+      'We look forward to your visit!'
+    })
+    console.log('mailt sent successfully: ', result)
+  } catch (err) {
+    console.log('could not send email: ' + err.message)
+  }
+}
+
+main()
