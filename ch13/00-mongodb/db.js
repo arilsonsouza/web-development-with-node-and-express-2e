ed3ac4629153e7fb1b8e4f1937cf98cb8ae65651
Arilson Souza <arilsons61@gmail.com>
2020-01-21 22:07:41 -0300

    ch13 - Setting Up MongoDB - Creating Schemas and Models

--- ch13/00-mongodb/db.js
+++ ch13/00-mongodb/db.js
@@ -1,3 +1,23 @@
+const mongoose = require('mongoose')
+const { credentials } = require('./config')
+
+if (!credentials.connectionString) {
+  console.log('MongoDB connection string missing')
+  process.exit(1)
+}
+
+mongoose.connect(credentials.connectionString, {
+  useNewUrlParser: true
+})
+const db = mongoose.connection
+db.on('error', err => {
+  console.error('MongoDB error: ' + err.message)
+  process.exit(1)
+})
+db.once('open', () => console.log('MongoDB connection established'))
+
+const Vacation = require('./models/vacation')
+
 module.exports = {
   getVacations: async (options = {}) => {
       const vacations = [
