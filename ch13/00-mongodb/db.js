d927974e6188ae8f378f9361d4f74bceea57bd60
Arilson Souza <arilsons61@gmail.com>
2020-01-21 22:08:40 -0300

    ch13 - Seeding Initial Data

--- ch13/00-mongodb/db.js
+++ ch13/00-mongodb/db.js
@@ -18,6 +18,56 @@ db.once('open', () => console.log('MongoDB connection established'))
 
 const Vacation = require('./models/vacation')
 
+Vacation.find((err, vacations) => {
+  if(err) return console.error(err)
+  if(vacations.length) return
+
+  new Vacation({
+    name: 'Hood River Day Trip',
+    slug: 'hood-river-day-trip',
+    category: 'Day Trip',
+    sku: 'HR199',
+    description: 'Spend a day sailing on the Columbia and ' + 
+      'enjoying craft beers in Hood River!',
+    price: 99.95,
+    tags: ['day trip', 'hood river', 'sailing', 'windsurfing', 'breweries'],
+    inSeason: true,
+    maximumGuests: 16,
+    available: true,
+    packagesSold: 0,
+  }).save()
+
+  new Vacation({
+    name: 'Oregon Coast Getaway',
+    slug: 'oregon-coast-getaway',
+    category: 'Weekend Getaway',
+    sku: 'OC39',
+    description: 'Enjoy the ocean air and quaint coastal towns!',
+    price: 269.95,
+    tags: ['weekend getaway', 'oregon coast', 'beachcombing'],
+    inSeason: false,
+    maximumGuests: 8,
+    available: true,
+    packagesSold: 0,
+  }).save()
+
+  new Vacation({
+      name: 'Rock Climbing in Bend',
+      slug: 'rock-climbing-in-bend',
+      category: 'Adventure',
+      sku: 'B99',
+      description: 'Experience the thrill of climbing in the high desert.',
+      price: 289.95,
+      tags: ['weekend getaway', 'bend', 'high desert', 'rock climbing'],
+      inSeason: true,
+      requiresWaiver: true,
+      maximumGuests: 4,
+      available: false,
+      packagesSold: 0,
+      notes: 'The tour guide is currently recovering from a skiing accident.',
+  }).save()
+})
+
 module.exports = {
   getVacations: async (options = {}) => {
       const vacations = [
